11ad1e8beb81c663b74c9b562b54caf71c5f36f1
Petr Pichugin <petr.pichugin@gmail.com>
2021-11-25 17:39:26 +0500

    Variable.java v.1.3. added

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,20 +1,20 @@
 public class Variable {
-    
+
     public static void main(String[] args) {
-    // Объявляем переменные примитивных типов данных
-    byte quantityDisks = 1;
-    long ssdSizeBites = 510_770_802_688L;
-    int ssdSizeGb = (int) (ssdSizeBites / 1000000000);
-    int quantityFiles = 437_056;
-    int quantityFolders = 154064;
-    short ramGb = 16;
-    double ramGbMaxUse = 15.9;
-    float cpuBaseFrequencyHz = 1.8F;
-    double cpuMaxFrequencyHz = 4.0D;
-    byte cpuCores = 4;
-    char symbol = '!';
-    char symbolCr = '\n';
-    boolean isMine = true;
+        // Объявляем переменные примитивных типов данных
+        byte quantityDisks = 1;
+        long ssdSizeBites = 510_770_802_688L;
+        int ssdSizeGb = (int) (ssdSizeBites / 1000000000);
+        int quantityFiles = 437_056;
+        int quantityFolders = 154064;
+        short ramGb = 16;
+        double ramGbMaxUse = 15.9;
+        float cpuBaseFrequencyHz = 1.8F;
+        double cpuMaxFrequencyHz = 4.0D;
+        byte cpuCores = 4;
+        char symbol = '!';
+        char symbolCr = '\n';
+        boolean isMine = true;
 
         //Выводим переменные на экран
         System.out.println("Параметры персонального компьютера:" + symbolCr);
@@ -27,10 +27,11 @@ public class Variable {
         System.out.println("Тип процессора: " + '\t' + '\t' + "Intel Core i7-8550U");
         System.out.println("Частота: " + '\t' + '\t' + '\t' + cpuBaseFrequencyHz + " Гц. (Макс. частота: " + cpuMaxFrequencyHz + " Гц.)");
         System.out.println("Число ядер: " + '\t' + '\t' + '\t' + cpuCores + symbolCr);
-            if (isMine) {
-                System.out.println("Это мой ПК" + symbol);
-            } else {
-                System.out.println("Это чужой ПК" + symbol);
-            }
+
+        if (isMine) {
+            System.out.println("Это мой ПК" + symbol);
+        } else {
+            System.out.println("Это чужой ПК" + symbol);
+        }
     }
 }
