e8daa1d14f68729fa5ff7d90c634dece8e932afb
Petr Pichugin <petr.pichugin@gmail.com>
2021-11-28 15:29:47 +0500

    Changes to code formatting

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,37 +1,34 @@
 public class ConditionalStatement {
 
     public static void main(String[] args) {
-
-        // https://github.com/ichimax/startjava/blob/master/lesson%201.md#-7-ветвление-if-if-else
         int age = 22;
+        if (age > 20) {
+            System.out.println("Возраст " + age + " (лет) больше 20");
+        }
+
         boolean isMale = true;
+        if (isMale) {
+            System.out.println("Мужской пол.");
+        }
+
+        if (!isMale) {
+            System.out.println("Женский пол.");
+        }
+
         double height = 1.85;
+        if (height < 1.80) {
+            System.out.println("Рост - средний или ниже среднего: " + height);
+        } else {
+            System.out.println("Рост - высокий: " + height);
+        }
+
         char firstLetterOfName = 'M';
-
-            if (age > 20) {
-                System.out.println("Возраст " + age + " (лет) больше 20");
-            }
-
-            if (isMale) {
-                System.out.println("Мужской пол.");
-            }
-
-            if (!isMale) {
-                System.out.println("Женский пол.");
-            }
-
-            if (height < 1.80) {
-                System.out.println("Рост - средний или ниже среднего: " + height);
-            } else {
-                System.out.println("Рост - высокий: " + height);
-            }
-
-            if (firstLetterOfName == 'M') {
-                System.out.println("Вариант 1. Первая буква имени: " + firstLetterOfName);
-            } else if (firstLetterOfName == 'I') {
-                System.out.println("Вариант 2. Первая буква имени: " + firstLetterOfName);
-            } else {
-                System.out.println("Вариант 3. Первая буква имени вне диапазона проверки: " + firstLetterOfName);
-            }
+        if (firstLetterOfName == 'M') {
+            System.out.println("Вариант 1. Первая буква имени: " + firstLetterOfName);
+        } else if (firstLetterOfName == 'I') {
+            System.out.println("Вариант 2. Первая буква имени: " + firstLetterOfName);
+        } else {
+            System.out.println("Вариант 3. Первая буква имени вне диапазона проверки: " + firstLetterOfName);
+        }
     }
 }
--- Lesson_1/Cycle.java
+++ Lesson_1/Cycle.java
@@ -1,8 +1,8 @@
 public class Cycle {
 
     public static void main(String[] args) {
-
-        int i; // определяем переменную для счетчика циклов
+        // определяем переменную для счетчика циклов
+        int i;
         System.out.println("Д/З по циклам.");
         System.out.println("Часть 1. Вывести все числа используя цикл for от 0 до 20:");
         for (i = 0; i <= 20; i++) {
@@ -14,27 +14,29 @@ public class Cycle {
         i = 6;
         while (i >= -6) {
             System.out.print(i + " ");
-            i = i - 2;
+            i -= 2;
         }
 
-        i = 10; // устанавливаем начальное значение для цикла
-        int lastNumber = 20; // конечное значение цикла, чтобы унифицировать алгоритм
-        int oddSumNumbers = 0;
+        // устанавливаем начальное значение для цикла
+        i = 10;
+        // конечное значение цикла, чтобы унифицировать алгоритм
+        int lastNumber = 20;
+        int sumOdd = 0;
         System.out.println(" " + '\n');
         System.out.println("Часть 3. Подсчитать сумму нечетных чисел используя цикл do-while в диапазоне [" + i +", " + lastNumber +"]");
         do {
             if (i % 2 == 1) {
                 System.out.print(i);
-                oddSumNumbers = oddSumNumbers + i;
-                    // если это последнее значение выборки, то ставим "=", иначе ставим "+" между цифрами
-                    if (i == lastNumber | i == lastNumber - 1) {
-                        System.out.print(" = ");
-                    } else {
-                        System.out.print(" + ");
-                    }
+                sumOdd += i;
+                // если это последнее значение выборки, то ставим "=", иначе ставим "+" между цифрами
+                if (i == lastNumber | i == lastNumber - 1) {
+                    System.out.print(" = ");
+                } else {
+                    System.out.print(" + ");
+                }
             }
         i++;
         } while (i <= lastNumber);
-        System.out.print(oddSumNumbers);
+        System.out.print(sumOdd);
     }
 }
