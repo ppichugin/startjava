371d55d0fa0811da321122c62b6c14003d1ed9b1
Petr Pichugin <petr.pichugin@gmail.com>
2021-12-02 16:16:24 +0500

    'MyFirstGame' - variable names were changed

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -2,23 +2,23 @@ public class MyFirstGame {
 
     public static void main(String[] args) {
         // "загадываем" число в диапазоне [0, 100]
-        int numberMin = 0;
-        int numberMax = 100;
-        int numberFromComputer = 1;
-        int numberFromPlayer = 50;
+        int minRange = 0;
+        int maxRange = 100;
+        int targetNumber = 10;
+        int playerAnswer = 50;
         do {
-            if (numberFromPlayer > numberFromComputer) {
-                System.out.println("Данное число (" + numberFromPlayer + ") больше того, что загадал компьютер");
+            if (playerAnswer > targetNumber) {
+                System.out.println("Данное число (" + playerAnswer + ") больше того, что загадал компьютер");
                 // сокращаем верхний диапазон поиска
-                numberMax = numberFromPlayer;
-                numberFromPlayer = (numberMin + numberMax) / 2;
-            } else if (numberFromPlayer < numberFromComputer) {
-                System.out.println("Данное число (" + numberFromPlayer + ") меньше того, что загадал компьютер");
+                maxRange = playerAnswer;
+                playerAnswer = (minRange + maxRange) / 2;
+            } else if (playerAnswer < targetNumber) {
+                System.out.println("Данное число (" + playerAnswer + ") меньше того, что загадал компьютер");
                 // повышаем нижний диапазон поиска
-                numberMin = numberFromPlayer;
-                numberFromPlayer = (numberMin + numberMax + 1) / 2;
+                minRange = playerAnswer;
+                playerAnswer = (minRange + maxRange + 1) / 2;
             }
-        } while (numberFromPlayer != numberFromComputer);
-        System.out.println("Поздравляю, число угадано! " + numberFromPlayer);
+        } while (playerAnswer != targetNumber);
+        System.out.println("Поздравляю, число угадано! " + playerAnswer);
     }
 }
