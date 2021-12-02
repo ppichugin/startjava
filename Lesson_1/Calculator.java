9ca61e84202a9490fc315cab2e2fc2a4fabef19f
Petr Pichugin <petr.pichugin@gmail.com>
2021-12-02 16:15:12 +0500

    'Calculator' - variable names and operation for division were changed

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -2,52 +2,46 @@ public class Calculator {
 
     public static void main(String[] args) {
         // определяем переменные для цифр и математической операции (+, -, *, /, ^, %)
-        int number1 = 175;
+        int number1 = 17;
         int number2 = 21;
-        char symbolOfOperation = '%';
-        System.out.println("Задание: " + number1 + " " + symbolOfOperation + " " + number2);
+        char mathSign = '/';
+        System.out.println("Задание: " + number1 + " " + mathSign + " " + number2);
         double result = 0;
-        switch (symbolOfOperation) {
+        switch (mathSign) {
             case '+':
                 System.out.println("Выполняем сложение");
                 result = number1 + number2;
                 System.out.println(number1 + " + " + number2 + " = " + (int) result);
                 break;
-
             case '-':
                 System.out.println("Выполняем вычитание");
                 result = number1 - number2;
                 System.out.println(number1 + " - " + number2 + " = " + (int) result);
                 break;
-
             case '*':
                 System.out.println("Выполняем умножение");
                 result = number1 * number2;
                 System.out.println(number1 + " * " + number2 + " = " + (int) result);
                 break;
-
             case '/':
                 System.out.println("Выполняем деление");
-                result = number1 / number2;
+                result = (double)number1 / (double)number2;
                 System.out.println(number1 + " / " + number2 + " = " + result);
                 break;
-
             case '^':
                 System.out.println("Выполняем возведение в степень");
                 // используем тип Long так как возведение в степень может иметь большие значения
-                long resultLong = 1;
+                long resultDivision = 1;
                 for (int j = 1; j <= number2; j++) {
-                    resultLong = resultLong * number1;
+                    resultDivision *= number1;
                 }
-                System.out.println(number1 + " ^ " + number2 + " = " + resultLong);
+                System.out.println(number1 + " ^ " + number2 + " = " + resultDivision);
                 break;
-
             case '%':
                 System.out.println("Выполняем деление по модулю");
                 result = number1 % number2;
                 System.out.println(number1 + " % " + number2 + " = " + (int) result);
                 break;
-
             default:
                 System.out.println("Математическая операция невозможна. Недопустимый оператор.");
                 break;
