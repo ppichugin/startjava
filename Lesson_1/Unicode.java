0294d2bb7ec9db515c3ce1b756b75fb6a7536aad
Petr Pichugin <petr.pichugin@gmail.com>
2021-12-05 21:35:14 +0500

    Support of Unicode to UTF-8 was added through PrintStream

--- Lesson_1/Unicode.java
+++ Lesson_1/Unicode.java
@@ -1,19 +1,23 @@
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
 public class Unicode {
 
     public static void main(String[] args) {
-        char symbol = ' ';
-        // символы ASCII
+
         System.out.println("Символы от 33 до 126");
-        for (int i = 33; i <= 126; i++) {
-            symbol = (char)i;
-            System.out.println(i + "\t" + symbol);
-            //System.out.print(charAt(i) + " ");
+        for (char i = 33; i <= 126; i++) {
+            System.out.print(i);
         }
-        // символы Unicode
+        System.out.println(" " + "\n");
         System.out.println("Символы Unicode от 9398 до 10178");
-        for (int i = 9398; i <=10178; i++) {
-            symbol = (char)i;
-            System.out.println(i + "\t" + symbol);
+        try {
+            PrintStream outStream = new PrintStream(System.out, true, "UTF-8");
+            for (char c = 9398; c <= 10178; c++) {
+                outStream.print(c);
+            }
+        } catch(UnsupportedEncodingException e){
+            System.out.println("Caught exception: " + e.getMessage());
         }
     }
 }
