62617b8b920b65016847a9e0d20da1982648ed6c
Petr Pichugin <petr.pichugin@gmail.com>
2021-12-07 21:12:49 +0500

    HW_2. New file 'Person.java' added

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    boolean isMale = true;
+    String name = "Pavel";
+    int height = 180;
+    int weight = 80;
+    int age = 35;
+
+    public void walk() {
+        System.out.println("I'm walking.");
+    }
+
+    public boolean seat() {
+        return true;
+    }
+
+    public void run() {
+        System.out.println("I'm running.");
+    }
+
+    public void speak() {
+        System.out.println("I'm speaking.");
+    }
+
+    public void learningJava() {
+        System.out.println("I'm learning Java.");
+    }
+}
