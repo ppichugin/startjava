8a1ea35baaede87a4e7cddd380d9fe6f195c5af2
Petr Pichugin <petr.pichugin@gmail.com>
2021-12-13 16:01:15 +0500

    Calculator & CalculatorTest - refactoring was done

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -1,62 +1,63 @@
 public class Calculator {
 
-        private int number1;
-        private int number2;
-        private char mathSign;
-        private double result;
+    private int number1;
+    private int number2;
+    private char mathSign;
+    private int result;
 
-        public void setNumber1 (int number1) {
-            this.number1 = number1;
-        }
+    public void setNumber1(int number1) {
+        this.number1 = number1;
+    }
 
-        public void setNumber2 (int number2) {
-            this.number2 = number2;
-        }
+    public void setNumber2(int number2) {
+        this.number2 = number2;
+    }
 
-        public void setMathSign (char mathSign) {
-            this.mathSign = mathSign;
-        }
+    public void setMathSign(char mathSign) {
+        this.mathSign = mathSign;
+    }
 
-        public void calc() {
-            System.out.println("Задание: " + number1 + mathSign + number2);
-            switch (mathSign) {
-            case '+':
-                System.out.println("Выполняем сложение");
-                result = number1 + number2;
-                System.out.println(number1 + " + " + number2 + " = " + (int) result);
-                break;
-            case '-':
-                System.out.println("Выполняем вычитание");
-                result = number1 - number2;
-                System.out.println(number1 + " - " + number2 + " = " + (int) result);
-                break;
-            case '*':
-                System.out.println("Выполняем умножение");
-                result = number1 * number2;
-                System.out.println(number1 + " * " + number2 + " = " + (int) result);
-                break;
-            case '/':
-                System.out.println("Выполняем деление");
-                result = (double)number1 / (double)number2;
-                System.out.println(number1 + " / " + number2 + " = " + result);
-                break;
-            case '^':
-                System.out.println("Выполняем возведение в степень");
-                // используем тип Long так как возведение в степень может иметь большие значения
-                long resultDivision = 1;
-                for (int j = 1; j <= number2; j++) {
-                    resultDivision *= number1;
-                }
-                System.out.println(number1 + " ^ " + number2 + " = " + resultDivision);
-                break;
-            case '%':
-                System.out.println("Выполняем деление по модулю");
-                result = number1 % number2;
-                System.out.println(number1 + " % " + number2 + " = " + (int) result);
-                break;
-            default:
-                System.out.println("Математическая операция невозможна. Недопустимый оператор.");
-                break;
+    public void calc() {
+        System.out.println("Задание: " + number1 + mathSign + number2);
+        switch (mathSign) {
+        case '+':
+            System.out.println("Выполняем сложение");
+            result = number1 + number2;
+            System.out.println(number1 + " + " + number2 + " = " + result);
+            break;
+        case '-':
+            System.out.println("Выполняем вычитание");
+            result = number1 - number2;
+            System.out.println(number1 + " - " + number2 + " = " + result);
+            break;
+        case '*':
+            System.out.println("Выполняем умножение");
+            result = number1 * number2;
+            System.out.println(number1 + " * " + number2 + " = " + result);
+            break;
+        case '/':
+            System.out.println("Выполняем деление");
+            // используем тип double, чтобы сохранить знаки после запятой
+            double resultDivision = (double)number1 / (double)number2;
+            System.out.println(number1 + " / " + number2 + " = " + resultDivision);
+            break;
+        case '^':
+            System.out.println("Выполняем возведение в степень");
+            // используем тип Long так как возведение в степень может иметь большие значения
+            long resultExponentiaton = 1;
+            for (int j = 1; j <= number2; j++) {
+                resultExponentiaton *= number1;
             }
+            System.out.println(number1 + " ^ " + number2 + " = " + resultExponentiaton);
+            break;
+        case '%':
+            System.out.println("Выполняем деление по модулю");
+            result = number1 % number2;
+            System.out.println(number1 + " % " + number2 + " = " + result);
+            break;
+        default:
+            System.out.println("Математическая операция невозможна. Недопустимый оператор.");
+            break;
         }
+    }
 }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -4,32 +4,21 @@ public class CalculatorTest {
 
     public static void main(String[] args) {
         Scanner scanInput = new Scanner(System.in);
-        Scanner scanCheck = new Scanner(System.in);
-        boolean continueCalculation = true;
-        String answer ="";
-        while (continueCalculation) {
+        String answer ="yes";
+        while (answer.equals("yes")) {
+            Calculator calc = new Calculator();
             System.out.println("Введите первое число: ");
-            int num1 = scanInput.nextInt();
-            System.out.println("Введите знак математической операции число: ");
-            char sign = scanInput.next().charAt(0);
+            calc.setNumber1(scanInput.nextInt());
+            System.out.println("Введите знак математической операции: ");
+            calc.setMathSign(scanInput.next().charAt(0));
             System.out.println("Введите второе число: ");
-            int num2 = scanInput.nextInt();
-            Calculator calcOne = new Calculator();
-            calcOne.setNumber1(num1);
-            calcOne.setNumber2(num2);
-            calcOne.setMathSign(sign);
-            calcOne.calc();
+            calc.setNumber2(scanInput.nextInt());
+            scanInput.nextLine();
+            calc.calc();
             do {
                 System.out.println("Хотите продолжить вычисления? [yes/no]:");
-                answer = scanCheck.nextLine();
-                if (answer.equals("yes") | answer.equals("no")) {
-                    break;
-                }
-            } while (true);
-            if (answer.equals("no")) {
-                continueCalculation = false;
-            }
+                answer = scanInput.nextLine();
+            } while (!(answer.equals("yes") | answer.equals("no")));
         }
-
     }
 }
