d54f15bbe9a31346c15b3c1cd242e10fbe4a0c31
Petr Pichugin <petr.pichugin@gmail.com>
2021-12-15 17:00:12 +0500

    CalculatorTest.java has been corrected

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -4,32 +4,22 @@ public class CalculatorTest {
 
     public static void main(String[] args) {
         Scanner scanInput = new Scanner(System.in);
+        Calculator calc = new Calculator();
         String answer = "yes";
-        outer:
-        while (true) {
-            if (!(answer.equals("yes") | answer.equals("no"))) {
-                System.out.println("Хотите продолжить вычисления? [yes/no]:");
-                answer = scanInput.nextLine();
-                continue outer;
-            } else if (answer.equals("no")) {
-                break;
+        do {
+            if (answer.equals("yes")) {
+                System.out.println("Введите первое число: ");
+                calc.setNumber1(scanInput.nextInt());
+                System.out.println("Введите знак математической операции: ");
+                calc.setMathSign(scanInput.next().charAt(0));
+                System.out.println("Введите второе число: ");
+                calc.setNumber2(scanInput.nextInt());
+                scanInput.nextLine();
+                calc.calc();
             }
-            Calculator calc = new Calculator();
-            System.out.println("Введите первое число: ");
-            calc.setNumber1(scanInput.nextInt());
-            System.out.println("Введите знак математической операции: ");
-            calc.setMathSign(scanInput.next().charAt(0));
-            System.out.println("Введите второе число: ");
-            calc.setNumber2(scanInput.nextInt());
-            scanInput.nextLine();
-            calc.calc();
             System.out.println("Хотите продолжить вычисления? [yes/no]:");
             answer = scanInput.nextLine();
-/*            do {
-                System.out.println("Хотите продолжить вычисления? [yes/no]:");
-                answer = scanInput.nextLine();
-            } while (!(answer.equals("yes") | answer.equals("no")));*/
-        }
+        } while (!answer.equals("no"));
         scanInput.close();
     }
 }
