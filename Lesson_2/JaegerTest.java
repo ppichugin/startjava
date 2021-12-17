a4c7586d92ff58df378c10bc7529587493b3a8c1
Petr Pichugin <petr.pichugin@gmail.com>
2021-12-17 16:27:26 +0500

    JaegerTest has been corrected with printf()

--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -5,17 +5,17 @@ public class JaegerTest {
         Jaeger jaegerOne = new Jaeger("Crimson Typhoon", "Mark-4", "China", 3, 76.2f, 1.722f, 8, 6);
         Jaeger jaegerTwo = new Jaeger("Gipsy Danger", "Mark-3", "USA", 2, 79.25f, 1.980f, 8, 6);
         System.out.println("We have two Jaegers:");
-        System.out.println("Model name:" + "\t" + jaegerOne.getModelName() + "\t\t" + jaegerTwo.getModelName());
-        System.out.println("Mark version:" + "\t" + jaegerOne.getMark() + "\t\t\t" + jaegerTwo.getMark());
-        System.out.println("Origin:" + "\t\t" + jaegerOne.getOrigin() + "\t\t\t" + jaegerTwo.getOrigin());
-        System.out.println("Rangers:" + "\t" + jaegerOne.getRangers() + "\t\t\t" + jaegerTwo.getRangers());
-        System.out.println("Height:" + "\t\t" + jaegerOne.getHeight() + "\t\t\t" + jaegerTwo.getHeight());
-        System.out.println("Weight:" + "\t\t" + jaegerOne.getWeight() + "\t\t\t" + jaegerTwo.getWeight());
-        System.out.println("Strength:" + "\t" + jaegerOne.getStrength() + "\t\t\t" + jaegerTwo.getStrength());
-        System.out.println("Armor:" + "\t\t" + jaegerOne.getArmor() + "\t\t\t" + jaegerTwo.getArmor());
-        System.out.println("\n" + "What they can do:");
-        System.out.println("drift:" + "\t\t" + jaegerOne.drift() + "\t\t\t" + jaegerTwo.drift());
-        System.out.println("move:" + "\t\t" + jaegerOne.move() + "\t\t" + jaegerTwo.move());
-        System.out.println("scan KaiJu:" + "\t" + jaegerOne.scanKaiju() + "\t\t" + jaegerTwo.scanKaiju());
+        System.out.printf("%-20s %-20s %-20s %n", "Model name:", jaegerOne.getModelName(), jaegerTwo.getModelName());
+        System.out.printf("%-20s %-20s %-20s %n", "Mark version:", jaegerOne.getMark(), jaegerTwo.getMark());
+        System.out.printf("%-20s %-20s %-20s %n", "Origin:", jaegerOne.getOrigin(), jaegerTwo.getOrigin());
+        System.out.printf("%-20s %-20d %-20d %n", "Rangers:", jaegerOne.getRangers(), jaegerTwo.getRangers());
+        System.out.printf("%-20s %-20.2f %-20.2f %n", "Height:", jaegerOne.getHeight(), jaegerTwo.getHeight());
+        System.out.printf("%-20s %-20.3f %-20.3f %n", "Weight:", jaegerOne.getWeight(), jaegerTwo.getWeight());
+        System.out.printf("%-20s %-20d %-20d %n", "Strength:", jaegerOne.getStrength(), jaegerTwo.getStrength());
+        System.out.printf("%-20s %-20d %-20d %n\n", "Armor:", jaegerOne.getArmor(), jaegerTwo.getArmor());
+        System.out.println("What they can do:");
+        System.out.printf("%-20s %-20s %-20s %n", "drift:", jaegerOne.drift(), jaegerTwo.drift());
+        System.out.printf("%-20s %-20s %-20s %n", "move:", jaegerOne.move(), jaegerTwo.move());
+        System.out.printf("%-20s %-20s %-20s %n", "scan KaiJu:", jaegerOne.scanKaiju(), jaegerTwo.scanKaiju());
     }
 }
