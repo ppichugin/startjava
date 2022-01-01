54d529f921841b74bdec3c9bfdb0e128b0b852c0
Petr Pichugin <petr.pichugin@gmail.com>
2022-01-01 17:33:56 +0500

    New switch expression for JDK 15 & input validation

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,52 +2,66 @@ package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
 
-    private int number1;
-    private int number2;
-    private char mathSign;
+    private static int number1;
+    private static int number2;
+    private static char mathSign;
 
-    public void setNumber1(int number1) {
-        this.number1 = number1;
-    }
-
-    public void setNumber2(int number2) {
-        this.number2 = number2;
-    }
-
-    public void setMathSign(char mathSign) {
-        this.mathSign = mathSign;
-    }
-
-    public void calculate() {
-        System.out.print("Задание: " + number1 + mathSign + number2);
-        switch (mathSign) {
-            case '+':
-                System.out.println(" = " + (number1 + number2));
-                break;
-            case '-':
-                System.out.println(" = " + (number1 - number2));
-                break;
-            case '*':
-                System.out.println(" = " + (number1 * number2));
-                break;
-            case '/':
-                // используем тип double, чтобы сохранить знаки после запятой
-                System.out.println(" = " + ((double) number1 / (double) number2));
-                break;
-            case '^':
-                // используем тип Long так как возведение в степень может иметь большие значения
-                long resultExponentiaton = 1;
-                for (int j = 1; j <= number2; j++) {
-                    resultExponentiaton *= number1;
-                }
-                System.out.println(" = " + resultExponentiaton);
-                break;
-            case '%':
-                System.out.println(" = " + (number1 % number2));
-                break;
-            default:
-                System.out.println("\n" + "Математическая операция невозможна. Недопустимый оператор.");
-                break;
+    public static boolean setNumber1(double number1) {
+        if (number1 < 0) {
+            System.out.println("Введеное первое число отрицательное.");
+            return false;
         }
+        if (number1 != (int) number1 ) {
+            System.out.println("Введеное первое число не целое.");
+            return false;
+        }
+        Calculator.number1 = (int) number1;
+        return true;
+    }
+
+    public static boolean setNumber2(double number2) {
+        if (number2 < 0) {
+            System.out.println("Введеное второе число число отрицательное.");
+            return false;
+        }
+        if (number2 != (int) number2) {
+            System.out.println("Введеное второе число не целое.");
+            return false;
+        }
+        Calculator.number2 = (int) number2;
+        return true;
+    }
+
+    public static void setMathSign(char mathSign) {
+        Calculator.mathSign = mathSign;
+    }
+
+    public static String calculate() {
+
+        String result =
+            switch (mathSign) {
+                case '+' -> {
+                    yield String.valueOf(number1 + number2);
+                }
+                case '-' -> {
+                    yield String.valueOf(number1 - number2);
+                }
+                case '*' -> {
+                    yield String.valueOf(number1 * number2);
+                }
+                case '/' -> {
+                    // используем тип double, чтобы сохранить знаки после запятой
+                    yield String.valueOf((double) number1 / (double) number2);
+                }
+                case '^' -> {
+                    // используем тип Long так как возведение в степень может иметь большие значения
+                   yield String.valueOf((long) Math.pow(number1, number2));
+                }
+                case '%' -> {
+                    yield String.valueOf(number1 % number2);
+                }
+                default -> "Математическая операция невозможна. Недопустимый оператор.";
+            };
+        return result;
     }
 }
