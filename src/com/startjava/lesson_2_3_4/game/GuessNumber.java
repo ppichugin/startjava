c82b9e337461b2856200256d085113e84b167dc6
Petr Pichugin <petr.pichugin@gmail.com>
2022-01-05 12:36:38 +0500

    Methods added

--- src/com/startjava/lesson_2_3_4/game/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/game/GuessNumber.java
@@ -7,7 +7,7 @@ public class GuessNumber {
     private int secretNumber;
     private final Player[] players = new Player[3];
     private final String[] winners = new String[3];
-    private int[] sequence = new int[3];
+    private int[] orderOfPriority = new int[3];
 
     public GuessNumber(Player player1, Player player2, Player player3) {
         players[0] = player1;
@@ -23,37 +23,13 @@ public class GuessNumber {
             generateSecretNumber();
             System.out.println("\nКомпьютер загадал число.");
             System.out.println("=== РАУНД: " + (round + 1));
-
-            // жребий (перенести в отдельный метод)
-            System.out.println("Бросаем жребий для определения хода");
-            Random r = new Random();
-            for (int i = 0; i < 3; i++) {
-                sequence[i] = r.nextInt(15) + 3;    // три костяшки: 18 баллов макс., 3 балла мин.
-                System.out.println("Игрок " + players[i].getName() + " получил " + sequence[i] + " очков");
-            }
-            int indexOfMax = 0;
-            int indexOfMin = 0;
-            for (int i = 1; i < 3; i++) {
-                if (sequence[i] > sequence[indexOfMax]) {
-                    indexOfMax = i;
-                } else if (sequence[i] < sequence[indexOfMin]) {
-                    indexOfMin = i;
-                }
-            }
-            int indexOfMid = (6 - ((indexOfMin + 1) + (indexOfMax + 1))) - 1;
-            System.out.println("Первым ходит: " + players[indexOfMax].getName());
-            System.out.println("Вторым ходит: " + players[indexOfMid].getName());
-            System.out.println("Третьим ходит: " + players[indexOfMin].getName());
-            sequence[0] = indexOfMax;
-            sequence[1] = indexOfMid;
-            sequence[2] = indexOfMin;
-            System.out.println("Индексы игроков по порядку: " + Arrays.toString(sequence));
-
+            // игроки бросают жребий
+            lot();
             // начало игры
-            for (int i = 0; i < 10; i++) {              // попытки
+            for (int i = 0; i < 10; i++) { // попытки
                 System.out.println("---- Попытка № " + (i + 1));
-                for (int j = 0; j < players.length; j++) {
-                    Player player = players[sequence[j]];         // ходы участников
+                for (int j = 0; j < players.length; j++) { // ходы участников
+                    Player player = players[orderOfPriority[j]];
                     inputNumber(player);
                     if (checkNumber(player)) {
                         winners[round] = player.getName();
@@ -62,32 +38,13 @@ public class GuessNumber {
                     }
                 }
             }
+            // выводим числа каждого игрока и подготавливаем массивы к следующему раунду
             for (Player player : players) {
                 setUp(player);
             }
         }
-
-        System.out.print("Победители в трех раундах: ");
-        for (String name : winners) {
-            System.out.print(name + " ");
-        }
-        System.out.println("");
-        int countPlayer1 = Collections.frequency(List.of(winners),players[0].getName());
-        int countPlayer2 = Collections.frequency(List.of(winners),players[1].getName());
-        int countPlayer3 = Collections.frequency(List.of(winners),players[2].getName());
-        System.out.println("Игрок 1 (" + players[0].getName() + ") выиграл " + countPlayer1 + " раз.");
-        System.out.println("Игрок 2 (" + players[1].getName() + ") выиграл " + countPlayer2 + " раз.");
-        System.out.println("Игрок 3 (" + players[2].getName() + ") выиграл " + countPlayer3 + " раз.");
-
-        if (countPlayer1 == countPlayer2 & countPlayer1 == countPlayer3) {
-            System.out.println("Победителя нет. Победила дружба! :)");
-        } else if (countPlayer1 > countPlayer2 & countPlayer1 > countPlayer3) {
-            System.out.println("Победил: " + players[0].getName());
-        } else if (countPlayer2 > countPlayer1 & countPlayer2 > countPlayer3) {
-            System.out.println("Победил: " + players[1].getName());
-        } else {
-            System.out.println("Победил: " + players[2].getName());
-        }
+        // определение победителя по результатам раундов
+        winnerSelection();
     }
 
     private void generateSecretNumber() {
@@ -122,9 +79,57 @@ public class GuessNumber {
         return false;
     }
 
-    public void setUp(Player player) {
+    private void setUp(Player player) {
         System.out.println("Числа игрока " + player.getName() + ": " + Arrays.toString(player.getGuessedNumbers()).replaceAll("\\D+", " "));
         player.resetAllNumbers();
         player.setAttempt(0);
+    }
+
+    private void lot() {
+        System.out.println("Бросаем жребий для определения хода");
+        Random r = new Random();
+        for (int i = 0; i < 3; i++) {
+            orderOfPriority[i] = r.nextInt(15) + 3;    // 18 max, 3 min
+            System.out.println("Игрок " + players[i].getName() + " получил: " + orderOfPriority[i] + " очков");
+        }
+        int indexOfMax = 0;
+        int indexOfMin = 0;
+        for (int i = 1; i < 3; i++) {
+            if (orderOfPriority[i] > orderOfPriority[indexOfMax]) {
+                indexOfMax = i;
+            } else if (orderOfPriority[i] < orderOfPriority[indexOfMin]) {
+                indexOfMin = i;
+            }
+        }
+        int indexOfMid = (6 - ((indexOfMin + 1) + (indexOfMax + 1))) - 1;
+        System.out.print("Первым ходит: " + players[indexOfMax].getName());
+        System.out.print(". Вторым ходит: " + players[indexOfMid].getName());
+        System.out.println(". Третьим ходит: " + players[indexOfMin].getName());
+        orderOfPriority[0] = indexOfMax;
+        orderOfPriority[1] = indexOfMid;
+        orderOfPriority[2] = indexOfMin;
+    }
+
+    private void winnerSelection() {
+        System.out.print("Победители в трех раундах: ");
+        for (String name : winners) {
+            System.out.print(name + " ");
+        }
+        System.out.println("");
+        int countPlayer1 = Collections.frequency(List.of(winners),players[0].getName());
+        int countPlayer2 = Collections.frequency(List.of(winners),players[1].getName());
+        int countPlayer3 = Collections.frequency(List.of(winners),players[2].getName());
+        System.out.println("Игрок 1 (" + players[0].getName() + ") выиграл " + countPlayer1 + " раз.");
+        System.out.println("Игрок 2 (" + players[1].getName() + ") выиграл " + countPlayer2 + " раз.");
+        System.out.println("Игрок 3 (" + players[2].getName() + ") выиграл " + countPlayer3 + " раз.");
+        if (countPlayer1 == countPlayer2 & countPlayer1 == countPlayer3) {
+            System.out.println("Победителя нет. Победила дружба! :)");
+        } else if (countPlayer1 > countPlayer2 & countPlayer1 > countPlayer3) {
+            System.out.println("Победил: " + players[0].getName());
+        } else if (countPlayer2 > countPlayer1 & countPlayer2 > countPlayer3) {
+            System.out.println("Победил: " + players[1].getName());
+        } else {
+            System.out.println("Победил: " + players[2].getName());
+        }
     }
 }
